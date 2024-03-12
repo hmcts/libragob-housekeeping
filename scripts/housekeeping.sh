4f460a35ffd7555c7d1720d8c58d959b8e1bf87c
Alex Bance <Alex.Bance@justice.gov.uk>
2024-03-12 19:22:23 +0000

    feat: added debug

--- scripts/housekeeping.sh
+++ scripts/housekeeping.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 echo "Running housekeeping tasks agains the LibraGoB database..."
 
-$event_username=$(cat /mnt/secrets/$KV_NAME/event-datasource-username)
-$event_password=$(cat /mnt/secrets/$KV_NAME/event-datasource-password)
-$event_url=$(cat /mnt/secrets/$KV_NAME/event-datasource-url)
-$event_host=$(echo $event_url | sed 's/jdbc:postgresql:\/\///' | sed 's/:5432//'| sed 's/\/.*//')
-$event_db=$(echo $event_url | sed 's/jdbc:postgresql:\/\///' | sed 's/:5432//' | sed 's/.*\///')
+event_username=$(cat /mnt/secrets/$KV_NAME/event-datasource-username)
+event_password=$(cat /mnt/secrets/$KV_NAME/event-datasource-password)
+event_url=$(cat /mnt/secrets/$KV_NAME/event-datasource-url)
+event_host=$(echo "$event_url" | sed 's/jdbc:postgresql:\/\///' | sed 's/:5432//' | sed 's/\/.*//')
+event_db=$(echo "$event_url" | sed 's/jdbc:postgresql:\/\///' | sed 's/:5432//' | sed 's/.*\///')
+
+echo "Connecting to $event_db database at $event_host"
 
 DB_HOST=$event_host
 DB_USER=$event_username
