890580d057bc0f9da88b504402abf50872681592
Alex Bance <Alex.Bance@justice.gov.uk>
2024-03-13 11:34:32 +0000

    feat: update script to output to csv

--- sql/update_requests_reporting.sql
+++ sql/update_requests_reporting.sql
@@ -1,4 +1,1 @@
-SELECT date_trunc('hour',CREATED_DATE) as "Day",count(*) as "COUNTupdates",sum(number_of_table_updates) as "SUMnumber_of_table_updates" from public.update_requests
-where created_date > current_date
-group by date_trunc('hour',CREATED_DATE)
-order by 1 desc;
+\copy (SELECT date_trunc('hour',CREATED_DATE) as "Day",count(*) as "COUNTupdates",sum(number_of_table_updates) as "SUMnumber_of_table_updates" from public.update_requests where created_date > current_date group by date_trunc('hour',CREATED_DATE) order by 1 desc;) To './output.csv' With CSV DELIMITER ',' HEADER
