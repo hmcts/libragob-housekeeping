3a20756d20f0363f04f906d3df0a2f28fe634cb8
Alex Bance <abance@bancey.xyz>
2024-05-23 14:17:03 +0100

    Update public_update_tables_housekeeping.sql

--- sql/public_update_tables_housekeeping.sql
+++ sql/public_update_tables_housekeeping.sql
@@ -1,4 +1,4 @@
 DELETE FROM public.table_updates
-WHERE created_date < CURRENT_TIMESTAMP - interval '7 days';
+WHERE created_date < CURRENT_TIMESTAMP - interval '11 days';
 DELETE FROM public.update_requests
 WHERE created_date < CURRENT_TIMESTAMP - interval '7 days';
