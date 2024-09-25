726fd64afd10b3ccc358985b120d8f3ee81a73ea
djporta <157360643+djporta@users.noreply.github.com>
2024-09-25 20:49:43 +0100

    Update 11dAZUREDB_AMD_row_counts_DAC_message_audit.sql
    /tmp/ams-reporting/

--- sql/11dAZUREDB_AMD_row_counts_DAC_message_audit.sql
+++ sql/11dAZUREDB_AMD_row_counts_DAC_message_audit.sql
@@ -1,1 +1,1 @@
-\copy (select count(*) from themis_dac.message_audit) To '/scripts/11dAZUREDB_AMD_row_counts_DAC_message_audit.csv' With CSV DELIMITER ','
+\copy (select count(*) from themis_dac.message_audit) To '/tmp/ams-reporting/11dAZUREDB_AMD_row_counts_DAC_message_audit.csv' With CSV DELIMITER ','
