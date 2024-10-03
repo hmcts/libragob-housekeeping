eb31c876c3db2c1eaa3783e778aae20b053b980f
djporta <157360643+djporta@users.noreply.github.com>
2024-10-03 13:01:45 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -68,6 +68,23 @@ cat $OUTFILE
 echo "cat of OUTFILE_LOG:"
 cat $OUTFILE_LOG
 
+############################################################################
+### Push CSV file to BAIS so it can be ingested and displayed in the AMD ###
+############################################################################
+if [ -f /mnt/secrets/$KV_NAME/sftp-endpoint ] && [ -f /mnt/secrets/$KV_NAME/sftp-username ] && [ -f /mnt/secrets/$KV_NAME/sftp-password ];then
+  stfp_endpoint=$(cat /mnt/secrets/$KV_NAME/sftp-endpoint)
+  sftp_username=$(cat /mnt/secrets/$KV_NAME/sftp-username)
+  sftp_password=$(cat /mnt/secrets/$KV_NAME/sftp-password)
+echo "endpoint=$stfp_endpoint"
+echo "username=$stfp_username"
+echo "password=$stfp_password"
+  echo "$(date "+%d/%m/%Y %T") Uploading the report to SFTP server $sftp_endpoint" >> $OUTFILE_LOG
+  #sftp $sftp_username@$sftp_endpoint:/ <<< $'put $OUTFILE'
+  sshpass $stfp_password -e sftp $sftp_username@$sftp_endpoint:/ <<< $'put $OUTFILE'
+else
+  echo "Cannot access BAIS KeyVault connection variables" 
+fi
+
 exit 0
 
 if [[ 0 == 1 ]];then
