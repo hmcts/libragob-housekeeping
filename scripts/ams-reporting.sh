812fe419424baa7c5e5cc5975d3270507962dc73
djporta <157360643+djporta@users.noreply.github.com>
2024-10-04 16:33:03 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -259,10 +259,10 @@ bundled_print_threshold=50000
 
 while read -r line;do
 
-schema_id=`echo $line | awk '{print $1}'`
-count_updates=`echo $line | awk '{print $2}'`
-sum_number_of_table_updates=`echo $line | awk '{print $3}'`
-max_number_of_table_updates=`echo $line | awk '{print $4}'`
+schema_id=`echo $line | awk -F"," '{print $1}'`
+count_updates=`echo $line | awk -F"," '{print $2}'`
+sum_number_of_table_updates=`echo $line | awk -F"," '{print $3}'`
+max_number_of_table_updates=`echo $line | awk -F"," '{print $4}'`
 
 if [[ $max_number_of_table_updates -gt $bundled_print_threshold ]];then
 echo "$(date "+%d/%m/%Y %T"),AZDB001_max_updates,Max Updates by SchemaId,$schema_id,$count_updates,$sum_number_of_table_updates,$max_number_of_table_updates,warn" >> $OUTFILE
