115d0ed295e797c40ace06f4c4c393b952d9da17
djporta <157360643+djporta@users.noreply.github.com>
2024-10-24 13:59:59 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -964,15 +964,15 @@ sftp_endpoint=$(cat /mnt/secrets/$KV_NAME/sftp-endpoint)
 sftp_username=$(cat /mnt/secrets/$KV_NAME/sftp-username)
 #ssh-keygen -t rsa -b 4096 -f /tmp/ams-reporting/ams-reporting -N 'djporta is passphrase'
 #ssh-keygen -t rsa -b 4096 -f /tmp/ams-reporting/ams-reporting -q
-#ssh-keygen -t rsa -b 4096 -f /tmp/ams-reporting/ams-reporting
+ssh-keygen -t rsa -b 4096 -f /tmp/ams-reporting/ams-reporting
 #ssh-keygen -v 3 -t rsa -b 4096 -f /tmp/ams-reporting/ams-reporting
 #sudo -u $sftp-username ssh-keygen -t rsa -b 4096 -f /tmp/ams-reporting/ams-reporting
-#mv /tmp/ams-reporting/ams-reporting.pub /tmp/ams-reporting/ams-reporting.pub.key
-#mv /tmp/ams-reporting/ams-reporting /tmp/ams-reporting/ams-reporting.pvt.key
-#echo "cat of ams-reporting.pub.key:"
-#cat /tmp/ams-reporting/ams-reporting.pub.key
-#echo "cat of ams-reporting.pvt.key:"
-#cat /tmp/ams-reporting/ams-reporting.pvt.key
+mv /tmp/ams-reporting/ams-reporting.pub /tmp/ams-reporting/ams-reporting.pub.key
+mv /tmp/ams-reporting/ams-reporting /tmp/ams-reporting/ams-reporting.pvt.key
+echo "cat of ams-reporting.pub.key:"
+cat /tmp/ams-reporting/ams-reporting.pub.key
+echo "cat of ams-reporting.pvt.key:"
+cat /tmp/ams-reporting/ams-reporting.pvt.key
 
 echo "$(date "+%d/%m/%Y %T") Uploading the CSV to BAIS" >> $OUTFILE_LOG
 #sftp -v -oidentityfile=/mnt/secrets/$KV_NAME/sftp-pvt-key ${sftp_username}@${sftp_endpoint} << EOF
