7968ab7f0f6e478746cba3b49b2e63555144cdf8
djporta <157360643+djporta@users.noreply.github.com>
2024-11-06 14:12:05 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ####################################################### This is the AMD AzureDB Healthcheck Script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ####################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.6_MAP.docx" is the latest version as of 18/10/2024
-echo "Script Version 11.9: force check 2"
+echo "Script Version 12.0: check 2 test"
 echo "Designed by Mark A. Porter"
 OPDIR="/tmp/ams-reporting/"
 mkdir $OPDIR
@@ -85,6 +85,9 @@ psql "sslmode=require host=${postgres_host} dbname=${postgres_db} port=${postgre
 echo "$(date "+%d/%m/%Y %T") SQL for Check #2 has been run" >> $OUTFILE_LOG
 
 if [ -e "${OPDIR}2AZUREDB_AMD_locked_keys.csv" ];then
+
+echo "cat of csv:"
+cat ${OPDIR}2AZUREDB_AMD_locked_keys.csv
 
 while read -r line;do
 
