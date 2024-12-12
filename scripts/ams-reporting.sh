68de7f084907555c49b1e797da76e63fb3931b06
djporta <157360643+djporta@users.noreply.github.com>
2024-12-12 17:40:18 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ####################################################### This is the AMD AzureDB Healthcheck Script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ####################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.7_MAP.docx" is the latest version as of 25/11/2024
-echo "Script Version 16.3 changed sftp server xx.25.xxx.x"
+echo "Script Version 16.4 tracert"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -1136,6 +1136,7 @@ fi
 
 echo "$(date "+%d/%m/%Y %T") Uploading the CSVs to BAIS" >> $OUTFILE_LOG
 #sftp -vvv -P ${sftp_port} -oStrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-rsa -i /tmp/ams-reporting/sftp-pvt-key ${sftp_username}@${sftp_endpoint} << EOF
+tracert $sftp_endpoint >> $OUTFILE_LOG
 echo "$(date "+%d/%m/%Y %T") Fired sftp conn"
 sftp -P ${sftp_port} -oStrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-rsa -i /tmp/ams-reporting/sftp-pvt-key ${sftp_username}@${sftp_endpoint} << EOF
 put $OUTFILE
@@ -1143,7 +1144,11 @@ put $OUTFILE_STATS
 quit
 EOF
 
+if [ $? -eq 0 ];then
 echo "$(date "+%d/%m/%Y %T") The CSVs have been successfully uploaded to BAIS" >> $OUTFILE_LOG
+else
+echo "$(date "+%d/%m/%Y %T") Connection to BAIS has timed out" >> $OUTFILE_LOG
+fi
 
 else
 
