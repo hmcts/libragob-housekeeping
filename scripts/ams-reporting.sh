e8b617fdb4f7caeaa720e70178c6a0654e6520cb
djporta <157360643+djporta@users.noreply.github.com>
2024-12-16 10:50:39 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.7_MAP.docx" is the latest version as of 25/11/2024
-echo "Script Version 16.7 MESSAGE_LOG tuning to 10M"
+echo "Script Version 16.7 44 270mins"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -376,11 +376,11 @@ if [[ $schema_id == 77 ]];then
 t_delta_threshold_mins=$((90*6))
 fi
 
-if [[ $schema_id == 99 ]] || [[ $schema_id == 130 ]];then
+if [[ $schema_id == 99 ]] || [[ $schema_id == 130 ]] || [[ $schema_id == 44 ]];then
 t_delta_threshold_mins=$((90*3))
 fi
 
-if [[ $schema_id == 44 ]] || [[ $schema_id == 61 ]];then
+if [[ $schema_id == 61 ]];then
 t_delta_threshold_mins=$((90*2))
 fi
 
