7b279d4b6cbc43f680280352cfd0136c200d1dd1
djporta <157360643+djporta@users.noreply.github.com>
2024-12-19 12:43:50 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -401,11 +401,11 @@ if [[ $schema_id == 135 ]];then
 t_delta_threshold_mins=$((90*4))
 fi
 
-if [[ $schema_id == 99 ]] || [[ $schema_id == 130 ]] || [[ $schema_id == 44 ]] || [[ $schema_id == 126 ]];then
+if [[ $schema_id == 99 ]] || [[ $schema_id == 130 ]] || [[ $schema_id == 44 ]] || [[ $schema_id == 126 ]] || [[ $schema_id == 112 ]] || [[ $schema_id == 105 ]];then
 t_delta_threshold_mins=$((90*3))
 fi
 
-if [[ $schema_id == 61 ]] || [[ $schema_id == 139 ]] || [[ $schema_id == 57 ]] || [[ $schema_id == 47 ]] || [[ $schema_id == 112 ]] || [[ $schema_id == 105 ]];then
+if [[ $schema_id == 61 ]] || [[ $schema_id == 139 ]] || [[ $schema_id == 57 ]] || [[ $schema_id == 47 ]];then
 t_delta_threshold_mins=$((90*2))
 fi
 
